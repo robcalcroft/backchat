32a00cb15fc1a7f56fc06461567cf6b7b2a52a12
Rob Calcroft <rob.calcroft@lloydsbanking.com>
2018-01-09 16:46:30 +0000

    Fixed to work with body

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const zlib = require('zlib');
 const url = require('url');
 const fs = require('fs');
 const minimist = require('minimist');
+const crypto = require('crypto');
+const stream = require('stream');
 
 const args = minimist(process.argv);
 const port = args.port || args.p || 10001;
@@ -26,13 +28,17 @@ const proxyServer = proxy.createProxyServer({});
 
 const log = message => console.log(`[${new Date().toISOString()}] - ${message}`);
 
-const getFileNameFromUrl = urlForFileName => `${Buffer.from(urlForFileName).toString('base64')}.json`;
+const getFileName = (fileIdentifier) => {
+  const hash = crypto.createHash('sha256');
+  hash.update(fileIdentifier);
+  return `${chatLocation}${hash.digest('hex')}.json`;
+};
 
-const saveResponse = (response) => {
+const saveResponse = (request, response) => {
   const rawBody = [];
   const contentEncodingHeader = response.headers['content-encoding'];
-  const { path } = response.socket._httpMessage;
   const isGzipped = !!contentEncodingHeader && contentEncodingHeader.includes('gzip');
+  const fileNameToWrite = getFileName(request.body || request.url);
 
   response.on('data', data => rawBody.push(data));
 
@@ -44,7 +50,7 @@ const saveResponse = (response) => {
       body = Buffer.concat(rawBody).toString();
     }
 
-    fs.writeFile(`${chatLocation}${getFileNameFromUrl(path)}`, JSON.stringify({
+    fs.writeFile(fileNameToWrite, JSON.stringify({
       recorded: Math.round(Date.now() / 1000),
       headers: response.headers,
       body,
@@ -52,53 +58,63 @@ const saveResponse = (response) => {
       if (error) {
         throw new Error(error.message);
       }
-      log(`Wrote ${getFileNameFromUrl(path)} for URL ${path}`);
+      log(`Wrote ${fileNameToWrite} for URL ${request.url}`);
     });
   });
 };
 
-proxyServer.on('proxyReq', (proxyRequest) => {
+proxyServer.on('proxyReq', (proxyRequest, request) => {
   // Some services require the `Host` header to be present
   proxyRequest.setHeader('Host', url.parse(proxyUrl).hostname);
+
+  // As we've already consumed the request body stream, we must now re-write the stream
+  // back into the proxied request
+  if (request.body) {
+    proxyRequest.write(request.body);
+  }
 });
 
-proxyServer.on('proxyRes', (proxyResponse) => {
+proxyServer.on('proxyRes', (proxyResponse, request) => {
   if (!disableRecording) {
-    return saveResponse(proxyResponse);
+    return saveResponse(request, proxyResponse);
   }
   return log(`URL ${proxyResponse.socket._httpMessage.path} not being cached as recording is disabled`);
 });
 
 http.createServer((request, response) => {
-  fs.readFile(`${chatLocation}${getFileNameFromUrl(request.url)}`, (error, data) => {
-    if (error || request.headers['x-backchat-override-record']) {
-      if (request.headers['x-backchat-override-record']) {
-        log(`URL ${request.url} being overridden to use proxy`);
-      } else {
-        log(`URL ${request.url} not found in cache, proxying to ${proxyUrl}`);
+  const body = [];
+
+  request.on('data', chunk => body.push(chunk));
+  request.on('end', () => {
+    request.body = Buffer.concat(body).toString();
+
+    fs.readFile(getFileName(request.body || request.url), (error, data) => {
+      if (error || request.headers['x-backchat-override-record']) {
+        if (request.headers['x-backchat-override-record']) {
+          log(`URL ${request.method} ${request.url} being overridden to use proxy`);
+        } else {
+          log(`URL ${request.method} ${request.url} not found in cache, proxying to ${proxyUrl}`);
+        }
+        response.setHeader(isBackchatHeaderName, 'no');
+        const parsedProxyUrl = url.parse(proxyUrl);
+        return proxyServer.web(request, response, {
+          target: {
+            host: parsedProxyUrl.hostname,
+            port: parsedProxyUrl.port,
+            https: parsedProxyUrl.protocol === 'https:',
+          },
+        });
       }
-      response.setHeader(isBackchatHeaderName, 'no');
-      const parsedProxyUrl = url.parse(proxyUrl);
-      return proxyServer.web(request, response, {
-        target: {
-          host: parsedProxyUrl.host,
-          port: parsedProxyUrl.port,
-          https: parsedProxyUrl.protocol === 'https:',
-        },
+      log(`URL ${request.method} ${request.url} found in cache returning contents`);
+
+      const chat = JSON.parse(data);
+      delete chat.headers['content-encoding'];
+      response.writeHead(200, {
+        ...chat.headers,
+        [isBackchatHeaderName]: 'yes',
       });
-    }
 
-    log(`URL ${request.url} found in cache returning contents`);
-
-    const chat = JSON.parse(data);
-
-    delete chat.headers['content-encoding'];
-
-    response.writeHead(200, {
-      ...chat.headers,
-      [isBackchatHeaderName]: 'yes',
+      return response.end(chat.body);
     });
-
-    return response.end(chat.body);
   });
 }).listen(port, () => log(`Backchat server for ${proxyUrl} running on port ${port}`));
